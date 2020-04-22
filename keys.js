fdf950373ca35fdfbf4e33547a3eeb31424c56c0
karla98 <riosdiazkm@gmail.com>
2020-04-21 21:45:06 -0500

    app completa login

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'fOPs1ZlZWr',
+        password: 'yy4VP1ikbV',
+        database: 'fOPs1ZlZWr'
+    }
+
+};
